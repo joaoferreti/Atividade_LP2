2b02df28be5a5e2bcd0f89ee2179c4f812844e87
João Vitor Ferreti Lippi <105336913+joaoferreti@users.noreply.github.com>
2022-11-22 15:29:04 -0300

    Update import win32com.client.py

--- import win32com.client.py
+++ import win32com.client.py
@@ -1,6 +1,6 @@
 import win32com.client
 
-e = win32com.client.Distpatch("Excel.Application") # Passando o Excel para à variável.
+e = win32com.client.Dispatch("Excel.Application") # Passando o Excel para a variável.
 e.Visible = 1 # Parâmetros
 e.Workbooks.Add() # Propriedade do Excel (workbook)
 e.Cells(1, 1).Value = "Hello World"
